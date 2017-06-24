f5aa80eb0f58bfa1db3c3e761e5a712a7da7f1ad
antham <hamonanth@gmail.com>
2017-06-24 06:49:22 +0200

    Rewrite to simplify

--- prompts.go
+++ prompts.go
@@ -38,112 +38,46 @@ type Prompts struct {
 	scenario      []Step
 }
 
-func (p *Prompts) renderPrompt(prompt Prompter) {
-	switch pr := prompt.(type) {
-	case PromptRenderer:
-		pr.PrintPrompt(prompt.GetPromptString())
-	default:
-		fmt.Fprintf(p.writer, "%s : \n", prompt.GetPromptString())
-	}
-}
+func (p *Prompts) parse() ([]string, Prompter, error) {
+	var nextPrompt Prompter
+	var inputs []string
+	var err error
 
-func (p *Prompts) renderError(prompt Prompter, err error) {
-	switch pr := prompt.(type) {
-	case ErrorRenderer:
-		pr.PrintError(err)
-	default:
-		fmt.Fprintf(p.writer, "%s\n", err.Error())
-	}
-}
+	switch prompt := p.currentPrompt.(type) {
+	case LinePrompter:
+		var input string
 
-func (p *Prompts) isMultilineEnd() (bool, error) {
-	bn, err := p.reader.ReadByte()
+		input, err = parseLine(p.reader, prompt)
 
-	if err == io.EOF {
-		return true, nil
-	}
-
-	if bn == '\n' {
-		return true, nil
-	}
-
-	if err := p.reader.UnreadByte(); err != nil {
-		return false, err
-	}
-
-	return false, nil
-}
-
-func (p *Prompts) parseMultipleLine(prompt MultilinePrompter) ([]string, error) {
-	inputs := []string{}
-
-	for {
-		input, err := p.reader.ReadString('\n')
-		input = strings.TrimRight(input, "\n")
-
-		if err != nil {
-			p.currentPrompt = p.prompts[prompt.GetNextOnError(err)]
-
-			return []string{}, err
+		if prompt.GetNextOnSuccess(input) != "" {
+			nextPrompt = p.prompts[prompt.GetNextOnSuccess(input)]
 		}
 
 		inputs = append(inputs, input)
+	case MultilinePrompter:
+		inputs, err = parseMultipleLine(p.reader, prompt)
 
-		end, err := p.isMultilineEnd()
-
-		if err != nil {
-			p.currentPrompt = p.prompts[prompt.GetNextOnError(err)]
-
-			return []string{}, err
-		}
-
-		if end {
-			break
+		if prompt.GetNextOnSuccess(inputs) != "" {
+			nextPrompt = p.prompts[prompt.GetNextOnSuccess(inputs)]
 		}
 	}
-
-	if err := prompt.Parse(inputs); err != nil {
-		p.currentPrompt = p.prompts[prompt.GetNextOnError(err)]
-
-		return inputs, err
-	}
-
-	if prompt.GetNextOnSuccess(inputs) == "" {
-		p.currentPrompt = nil
-
-		return inputs, nil
-	}
-
-	p.currentPrompt = p.prompts[prompt.GetNextOnSuccess(inputs)]
-
-	return inputs, nil
-}
-
-func (p *Prompts) parseLine(prompt LinePrompter) (string, error) {
-	input, err := p.reader.ReadString('\n')
-	input = strings.TrimRight(input, "\n")
 
 	if err != nil {
-		p.currentPrompt = p.prompts[prompt.GetNextOnError(err)]
-
-		return "", err
+		nextPrompt = p.prompts[p.currentPrompt.GetNextOnError(err)]
 	}
 
-	if err := prompt.Parse(input); err != nil {
-		p.currentPrompt = p.prompts[prompt.GetNextOnError(err)]
+	return inputs, nextPrompt, err
+}
 
-		return input, err
-	}
-
-	if prompt.GetNextOnSuccess(input) == "" {
-		p.currentPrompt = nil
-
-		return input, nil
-	}
-
-	p.currentPrompt = p.prompts[prompt.GetNextOnSuccess(input)]
-
-	return input, nil
+func (p *Prompts) appendScenario(promptString string, inputs []string, err error) {
+	p.scenario = append(
+		p.scenario,
+		Step{
+			promptString,
+			inputs,
+			err,
+		},
+	)
 }
 
 // AddLinePrompter add a new LinePrompter mapped to a given id
@@ -176,33 +110,102 @@ func (p *Prompts) Run() {
 		inputs := []string{}
 
 		prompt := p.currentPrompt
-		p.renderPrompt(prompt)
+		renderPrompt(p.writer, prompt)
 
-		switch lp := prompt.(type) {
-		case LinePrompter:
-			var input string
-			input, err = p.parseLine(lp)
-			inputs = append(inputs, input)
-		case MultilinePrompter:
-			inputs, err = p.parseMultipleLine(lp)
-
-		}
+		inputs, nextPrompt, err := p.parse()
 
 		if err != nil {
-			p.renderError(prompt, err)
+			renderError(p.writer, prompt, err)
 		}
 
-		p.scenario = append(
-			p.scenario,
-			Step{
-				prompt.GetPromptString(),
-				inputs,
-				err,
-			},
-		)
+		p.appendScenario(prompt.GetPromptString(), inputs, err)
 
-		if p.currentPrompt == nil {
+		if nextPrompt == nil {
 			return
 		}
+
+		p.currentPrompt = nextPrompt
+	}
+}
+
+func isMultilineEnd(reader *bufio.Reader) (bool, error) {
+	bn, err := reader.ReadByte()
+
+	if err == io.EOF {
+		return true, nil
+	}
+
+	if bn == '\n' {
+		return true, nil
+	}
+
+	if err := reader.UnreadByte(); err != nil {
+		return false, err
+	}
+
+	return false, nil
+}
+
+func parseMultipleLine(reader *bufio.Reader, prompt MultilinePrompter) ([]string, error) {
+	inputs := []string{}
+
+	for {
+		input, err := reader.ReadString('\n')
+		input = strings.TrimRight(input, "\n")
+
+		if err != nil {
+			return []string{}, err
+		}
+
+		inputs = append(inputs, input)
+
+		end, err := isMultilineEnd(reader)
+
+		if err != nil {
+			return []string{}, err
+		}
+
+		if end {
+			break
+		}
+	}
+
+	if err := prompt.Parse(inputs); err != nil {
+		return inputs, err
+	}
+
+	return inputs, nil
+}
+
+func parseLine(reader *bufio.Reader, prompt LinePrompter) (string, error) {
+	input, err := reader.ReadString('\n')
+	input = strings.TrimRight(input, "\n")
+
+	if err != nil {
+		return "", err
+	}
+
+	if err := prompt.Parse(input); err != nil {
+		return input, err
+	}
+
+	return input, nil
+}
+
+func renderPrompt(writer io.Writer, prompt Prompter) {
+	switch pr := prompt.(type) {
+	case PromptRenderer:
+		pr.PrintPrompt(prompt.GetPromptString())
+	default:
+		fmt.Fprintf(writer, "%s : \n", prompt.GetPromptString())
+	}
+}
+
+func renderError(writer io.Writer, prompt Prompter, err error) {
+	switch pr := prompt.(type) {
+	case ErrorRenderer:
+		pr.PrintError(err)
+	default:
+		fmt.Fprintf(writer, "%s\n", err.Error())
 	}
 }
